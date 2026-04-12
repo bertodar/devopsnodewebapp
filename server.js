9259c5577b62713dca8163d4e63bbc07f1a35990
Dario Bertoldo <bertodar@students.zhaw.ch>
2026-04-12 21:24:10 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+
+const PORT = 3001;
+const HOST = '0.0.0.0';
+
+app.get('/', (req, res) => {
+  res.send('Hello DevOps LN4 – Dario');
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Running on http://${HOST}:${PORT}`);
+});
