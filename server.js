9c6032a5cde8b85272eaf4ee5fc3fbefe72bfa55
Dario Bertoldo <bertodar@students.zhaw.ch>
2026-05-14 11:32:58 +0200

    Test auto deploy

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = 3001;
 const HOST = '0.0.0.0';
 
 app.get('/', (req, res) => {
-  res.send('Hello DevOps LN9 – Dario');
+  res.send('Hello DevOps LN9 – bertodar');
 });
 
 app.listen(PORT, HOST, () => {
